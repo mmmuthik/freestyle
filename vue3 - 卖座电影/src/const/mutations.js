e801b0dab7d91f8c0236fa4ec0fe1e1b807f04cb
邓贵羊 <72839246+mmmuthik@users.noreply.github.com>
2021-09-08 12:35:17 +0800

    删除之前的项目

--- vue3 - 卖座电影/src/const/mutations.js
+++
@@ -1,8 +0,0 @@
-// 设置底部状态栏的显隐
-export const SET_TAB_BAR_MUTATION = 'setTabBar'
-
-// 操作 正在热映 电影列表数据
-export const SET_CURRENT_FILMS_LIST_MUTATION = 'setCurrentFilmsList'
-
-// 操作 即将上映 电影列表数据
-export const SET_COMING_FILMS_LIST_MUTATION = 'setComingFilmsList'
--- vue3 - 卖座电影/vue.config.js
+++
@@ -1,9 +0,0 @@
-
-// 新建vue.config.js
-module.exports = {
-  devServer: {
-    port: 5555
-  }
-  // 不适用lint
-  // lintOnSave: false
-}
